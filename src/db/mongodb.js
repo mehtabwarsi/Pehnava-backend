6f0c9bd118378e7e642e3a07860de6fef5027a20
Mehtab alam warsi <83826593+mehtabwarsi@users.noreply.github.com>
2026-06-17 18:55:20 +0530

    Fix MongoDB connection URI by removing DB name
    momgo db

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -12,7 +12,7 @@ try {
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
         console.log(`\n mongo DB connected !! DB HOST: ${connectionInstance.connection.host}`)
          console.log(
             "DB Name:",
